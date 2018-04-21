459860530c7037225ddc5208b0858da7c8865785
Clement Hussenot <chussenota@gmail.com>
2018-04-21 17:18:24 +0200

    Bump the 0cean version to 0.1.1, work on shell purpose

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,20 @@ use std::time::Duration;
 use std::thread::sleep;
 use std::process::Command;
 
-static TEXT: &'static str = "Hello, Ocean!";
+static CMD: &'static str = "man vim";
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let _matches = App::from_yaml(yaml).get_matches();
-    if let Some(_matches) = _matches.subcommand_matches("serve") {
-        let _result = serve();
-    } else if let Some(_matches) = _matches.subcommand_matches("connect") {
-        connect();
+    if let Some(_matches) = _matches.subcommand_matches("server") {
+        let _result = server();
+    } else if let Some(_matches) = _matches.subcommand_matches("client") {
+        client();
     }
 }
 
-fn connect() {
+fn client() {
     let mut handle = Easy::new();
-
     loop {
         println!("Loop forever!");
         handle.url("http://127.0.0.1:3000").unwrap();
@@ -35,22 +34,20 @@ fn connect() {
            .arg("-c")
            .arg("man ed")
            .output()
-           .expect("failed to execute process");
-        println!("status: {}", output.status);
+           .expect("Failed to execute process");
         println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
         sleep(Duration::from_millis(1000));
     }
 }
 
-fn serve() -> Result<(), hyper::Error> {
+fn server() -> Result<(), hyper::Error> {
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let hello = const_service(service_fn(|_req|{
         Ok(Response::<hyper::Body>::new()
-            .with_header(ContentLength(TEXT.len() as u64))
+            .with_header(ContentLength(CMD.len() as u64))
             .with_header(ContentType::plaintext())
-            .with_body(TEXT))
+            .with_body(CMD))
     }));
 
     let server = Http::new().bind(&addr, hello)?;
